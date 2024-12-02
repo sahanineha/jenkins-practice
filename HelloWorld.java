bbf84175a92091ac9ded06f05f2df908fe7bffb2
sahanineha <117985690+sahanineha@users.noreply.github.com>
2024-12-02 16:59:08 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] s) {
+        for(int i = 0; i<=9; i++) {
+            System.out.println(i);
+            
+        }
+        System.out.println("Done");
+    }
+}
